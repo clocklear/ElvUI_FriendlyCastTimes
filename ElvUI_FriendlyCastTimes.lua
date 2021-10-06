6b80a7ab8accbec48b9a2b8e9c4a6a1f976edbd2
Cory Locklear <corylocklear@gmail.com>
2021-10-05 22:55:33 -0400

    Fix bug with incorrect function reference

--- ElvUI_FriendlyCastTimes.lua
+++ ElvUI_FriendlyCastTimes.lua
@@ -78,23 +78,23 @@ end
 function NP:Castbar_CustomDelayText(duration)
 	if self.channeling then
 		if self.channelTimeFormat == 'CURRENT' then
-			self.Time:SetFormattedText('%s |cffaf5050%.1f|r', NP:TimeFormat(abs(duration - self.max)), self.delay)
+			self.Time:SetFormattedText('%s |cffaf5050%.1f|r', TimeFormat(abs(duration - self.max)), self.delay)
 		elseif self.channelTimeFormat == 'CURRENTMAX' then
-			self.Time:SetFormattedText('%s / %s |cffaf5050%.1f|r', NP:TimeFormat(duration), NP:TimeFormat(self.max), self.delay)
+			self.Time:SetFormattedText('%s / %s |cffaf5050%.1f|r', TimeFormat(duration), TimeFormat(self.max), self.delay)
 		elseif self.channelTimeFormat == 'REMAINING' then
-			self.Time:SetFormattedText('%s |cffaf5050%.1f|r', NP:TimeFormat(duration), self.delay)
+			self.Time:SetFormattedText('%s |cffaf5050%.1f|r', TimeFormat(duration), self.delay)
 		elseif self.channelTimeFormat == 'REMAININGMAX' then
-			self.Time:SetFormattedText('%s / %s |cffaf5050%.1f|r', NP:TimeFormat(abs(duration - self.max)), NP:TimeFormat(self.max), self.delay)
+			self.Time:SetFormattedText('%s / %s |cffaf5050%.1f|r', TimeFormat(abs(duration - self.max)), TimeFormat(self.max), self.delay)
 		end
 	else
 		if self.castTimeFormat == 'CURRENT' then
-			self.Time:SetFormattedText('%s |cffaf5050%s %.1f|r', NP:TimeFormat(duration), '+', self.delay)
+			self.Time:SetFormattedText('%s |cffaf5050%s %.1f|r', TimeFormat(duration), '+', self.delay)
 		elseif self.castTimeFormat == 'CURRENTMAX' then
-			self.Time:SetFormattedText('%s / %s |cffaf5050%s %.1f|r', NP:TimeFormat(duration), NP:TimeFormat(self.max), '+', self.delay)
+			self.Time:SetFormattedText('%s / %s |cffaf5050%s %.1f|r', TimeFormat(duration), TimeFormat(self.max), '+', self.delay)
 		elseif self.castTimeFormat == 'REMAINING' then
-			self.Time:SetFormattedText('%s |cffaf5050%s %.1f|r', NP:TimeFormat(abs(duration - self.max)), '+', self.delay)
+			self.Time:SetFormattedText('%s |cffaf5050%s %.1f|r', TimeFormat(abs(duration - self.max)), '+', self.delay)
 		elseif self.castTimeFormat == 'REMAININGMAX' then
-			self.Time:SetFormattedText('%s / %s |cffaf5050%s %.1f|r', NP:TimeFormat(abs(duration - self.max)), NP:TimeFormat(self.max), '+', self.delay)
+			self.Time:SetFormattedText('%s / %s |cffaf5050%s %.1f|r', TimeFormat(abs(duration - self.max)), TimeFormat(self.max), '+', self.delay)
 		end
 	end
 end
@@ -102,23 +102,23 @@ end
 function NP:Castbar_CustomTimeText(duration)
 	if self.channeling then
 		if self.channelTimeFormat == 'CURRENT' then
-			self.Time:SetFormattedText('%s', NP:TimeFormat(abs(duration - self.max)))
+			self.Time:SetFormattedText('%s', TimeFormat(abs(duration - self.max)))
 		elseif self.channelTimeFormat == 'CURRENTMAX' then
-			self.Time:SetFormattedText('%s / %s', NP:TimeFormat(abs(duration - self.max)), NP:TimeFormat(self.max))
+			self.Time:SetFormattedText('%s / %s', TimeFormat(abs(duration - self.max)), TimeFormat(self.max))
 		elseif self.channelTimeFormat == 'REMAINING' then
-			self.Time:SetFormattedText('%s', NP:TimeFormat(duration))
+			self.Time:SetFormattedText('%s', TimeFormat(duration))
 		elseif self.channelTimeFormat == 'REMAININGMAX' then
-			self.Time:SetFormattedText('%s / %s', NP:TimeFormat(duration), NP:TimeFormat(self.max))
+			self.Time:SetFormattedText('%s / %s', TimeFormat(duration), TimeFormat(self.max))
 		end
 	else
 		if self.castTimeFormat == 'CURRENT' then
-			self.Time:SetFormattedText('%s', NP:TimeFormat(duration))
+			self.Time:SetFormattedText('%s', TimeFormat(duration))
 		elseif self.castTimeFormat == 'CURRENTMAX' then
-			self.Time:SetFormattedText('%s / %s', NP:TimeFormat(duration), NP:TimeFormat(self.max))
+			self.Time:SetFormattedText('%s / %s', TimeFormat(duration), TimeFormat(self.max))
 		elseif self.castTimeFormat == 'REMAINING' then
-			self.Time:SetFormattedText('%s', NP:TimeFormat(abs(duration - self.max)))
+			self.Time:SetFormattedText('%s', TimeFormat(abs(duration - self.max)))
 		elseif self.castTimeFormat == 'REMAININGMAX' then
-			self.Time:SetFormattedText('%s / %s', NP:TimeFormat(abs(duration - self.max)), NP:TimeFormat(self.max))
+			self.Time:SetFormattedText('%s / %s', TimeFormat(abs(duration - self.max)), TimeFormat(self.max))
 		end
 	end
 end
